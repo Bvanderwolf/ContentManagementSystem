714ba0799f3343d6f9ef9b598311acb75aee5f72
Benjamin <batsvanderwolf@yahoo.com>
2019-11-26 13:43:28 +0100

    Added submitForm function to send model data to github Rest APi

--- js/vueInputForm.js
+++ js/vueInputForm.js
@@ -25,6 +25,19 @@ new Vue({
     // check or uncheck all
     checkAll: function (event) {
       this.selection.features = event.target.checked ? this.features : [];
+    },
+
+    submitForm: function () {
+      console.log(this.message.text);
+      for (let i = 0; i < this.selection.features.length; i++) {
+        console.log(this.selection.features[i]);
+      }
+      console.log(this.selection.features);
+      console.log(this.selection.modeltype);
+
+      const xhttp = new XMLHttpRequest();
+      xhttp.open("GET", "https://github.com/login/oauth/authorize");
+      xhttp.send();
     }
   }
 });
