32ffa47b7b785abcac39d48568b08e1b13c6ec29
Oscar Hulshof <o.q.hulshof@quicknet.nl>
2019-11-27 15:09:36 +0100

    oauth test

--- js/vueHome.js
+++ js/vueHome.js
@@ -6,10 +6,29 @@ new Vue({
         // submit form handler
         loginGit: function() {
             const voetbaltaart = "338d81491cc7f65901b4";
-            const url = `https://github.com/login/oauth/authorize?client_id=${voetbaltaart}`;
-            window.location.assign(url);
-        
-            console.log("Hoi !");
+            const basketbaltaart = "ac44a104c1a628642561d1b7961644ce681df99c";
+            var url = ""
+
+            const queryParams = window.location.href.replace(window.location.origin + "/", "");
+            if (queryParams !== "") {
+                if (queryParams.includes("code")) {
+                    const code = queryParams.replace("?code=", "");
+
+                    url = "https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/access_token?code=${code}&client_id=${voetbaltaart}&client_secret=${basketbaltaart}";
+                    const xhttp = new XMLHttpRequest();
+                    xhttp.open("POST", url, true);
+                    xhttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+                    xhttp.setRequestHeader("Accept", "application/json");
+                    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                    xhttp.send();
+                    xhttp.responseType = "application/json";
+
+                    window.location.assign(window.location.origin + "inputForm.html" + xhttp.response);
+                }
+            } else {
+                url = "https://github.com/login/oauth/authorize?client_id=${voetbaltaart}";
+                window.location.assign(url);
+            }
         }
     }
 });
--- js/vueInputForm.js
+++ js/vueInputForm.js
@@ -31,9 +31,6 @@ new Vue({
             const text = this.message.text;
             const features = [];
             const modelType = this.selection.modeltype;
-            const voetbaltaart = "338d81491cc7f65901b4";
-            const basketbaaltaart = "ac44a104c1a628642561d1b7961644ce681df99c";
-            var url = ""
 
             console.log(this.message.text);
             for (let i = 0; i < this.selection.features.length; i++) {
@@ -42,24 +39,6 @@ new Vue({
             }
             console.log(this.selection.features);
             console.log(this.selection.modeltype);
-
-            const queryParams = window.location.href.replace(window.location.origin + "/", "");
-            if (queryParams !== "") {
-                if (queryParams.includes("code")) {
-                    const code = queryParams.replace("?code=", "");
-
-                    url = "https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/access_token?code=${code}&client_id=${voetbaltaart}&client_secret=${basketbaltaart}";
-                    const xhttp = new XMLHttpRequest();
-                    xhttp.open("POST", url, true);
-                    xhttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-                    xhttp.setRequestHeader("Accept", "application/json");
-                    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-                    xhttp.send();
-                }
-            } else {
-                url = "https://github.com/login/oauth/authorize?client_id=${voetbaltaart}";
-                window.location.assign(url);
-            }
         }
     }
 });
