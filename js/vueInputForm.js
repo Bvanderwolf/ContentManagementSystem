4a674ec5e4a87dc9c280f0367fb0371cc8a32a8b
Oscar Hulshof <OscarHulshof@users.noreply.github.com>
2019-12-03 13:22:14 +0100

    werkende test file upload
    test file kan geüpload worden. nog niet de file de geselecteerd is in de view, maar een test file. logica voor Github API werkt dus.

--- js/vueInputForm.js
+++ js/vueInputForm.js
@@ -27,18 +27,42 @@ new Vue({
             this.selection.features = event.target.checked ? this.features : [];
         },
 
+        /*
+        Upload file to Github using the Github Content API endpoint. 
+        See https://developer.github.com/v3/repos/contents/ for info.
+        
+        TODO
+        - Get file from view and upload file instead of test.txt (replace test.txt in 'url' constant)
+        - Get Base64 encoded content from file and replace 'content: "dgVzdA==" with file content
+        - Convert function to async await to improve performance
+        - Show message that upload was successful or show error message
+        */
         submitForm: function() {
-            const text = this.message.text;
-            const features = [];
-            const modelType = this.selection.modeltype;
+            // Get access token from current url
+            const queryParams = window.location.href.replace(window.location.origin + "/inputForm.html?", "");
+            var access_token = queryParams.replace("&scope=public_repo&token_type=bearer", "");
+            access_token = access_token.replace("access_token=", "");
 
-            console.log(this.message.text);
-            for (let i = 0; i < this.selection.features.length; i++) {
-                console.log(this.selection.features[i]);
-                features.push(this.selection.features[i]);
-            }
-            console.log(this.selection.features);
-            console.log(this.selection.modeltype);
+            // TODO: Replace test.txt with actual model from view
+            const url = "https://api.github.com/repos/bvanderwolf/bvanderwolf.github.io/contents/test.txt";
+            // TODO: Replace content with actual content from file to upload
+            const requestData = { message: this.message.text, content: "dGVzdA=="};
+
+            const xhttp = new XMLHttpRequest();
+            xhttp.open("PUT", url, true);
+            // Authorize
+            xhttp.setRequestHeader("Authorization", "token " + access_token);
+
+            xhttp.send(JSON.stringify(requestData));
+
+            // Wait until response from Github is fully recieved
+            xhttp.onreadystatechange = function() {
+                if (xhttp.readyState === 4) {
+                    console.log(xhttp.response);
+                }
+            } 
+            
+            // TODO: show user that upload was successful or show error message
         }
     }
 });
